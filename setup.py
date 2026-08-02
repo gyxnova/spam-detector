d3a2e0d9ce16288866c3c49d6054a78fd26af0ca
Gayatri <gsharma27084@gmail.com>
2026-08-02 11:49:49 +0530

    Initial project setup by cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End-to-end spam detection for SMS and Email using TF-IDF + ML with LLM-based explanations',
+    author='Gayatri',
+    license='',
+)
